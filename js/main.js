e866b7a6d41570ab5344eb15a4a043a003d8f88c
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-20 18:09:08 +0300

    implemented binary to string function

--- js/main.js
+++ js/main.js
@@ -5,11 +5,20 @@ function encrypt(){
 
 }
 
-
-const ToBinary = (str = '') => {
+// string to binary
+const stringToBinary = (str = '') => {
     let result = '';
     result = str.split('').map(char => {
         return char.charCodeAt(0).toString(2);
     }).join(' ');
     return result;
 };
+
+// binary to string
+function binaryToString(binary) {
+    binary = binary.split(' ');
+    let result = '';
+    result = binary.map(elem => String.fromCharCode(parseInt(elem, 2))).join("");
+    return result; 
+}
+

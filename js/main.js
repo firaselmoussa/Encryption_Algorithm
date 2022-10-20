d41dff069ee62d7d05dabdf7663a679070066709
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-20 19:38:43 +0300

    implemented RTE key &  testing samples

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
+const textToEncrypt0 = "testing";
+const textToEncrypt1 = "testing Xdhu gyug i iiuuuy ";
+const textToEncrypt2 = "testing Xdhu gyug i iiuuuy $#^$^ 42qHbljui86t 7rdfugvh iy4 94y9 y4982yh28r4hy23ieh20ue392ihrjhriwbdkbr uh iuhrfiufh4 orh oehfiebgk fh ihi";
 
-function encrypt(){
+RTE();
 
 
 
-}
+////////////////////////////////////////////////////
+
+
+
+// RANDOM TIMING ENCRYPTION 
+function RTE(){
+
+    // RTE-key
+    let RTE_key = Date.now();
+
+    
+
+
+}//end of RTE function
 
 // string to binary
 const stringToBinary = (str = '') => {

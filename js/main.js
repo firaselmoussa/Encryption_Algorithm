2a8748e0e2e7db769c758c1f9e533cc17fd76356
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-21 16:20:06 +0300

    implemented encryption & decryption html

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
-// const encrypt_btn = document.getElementById('encrypt_btn');
-// const encrypt_input = document.getElementById('encrypt_input');
-// const encryption_result = document.getElementById('encryption_result');
-
-// const decrypt_btn = document.getElementById('decrypt_btn');
-// const decrypt_input = document.getElementById('decrypt_input');
-// const RTE_key_input = document.getElementById('RTE_key_input');
-// const decryption_result = document.getElementById('decryption_result');
+const encrypt_btn = document.getElementById('encrypt_btn');
+const encrypt_input = document.getElementById('encrypt_input');
+const encryption_key = document.getElementById('encryption_key');
 
 
-// // 
-// encrypt_btn.addEventListener('click', ()=>{
-
-//     // encrypting input
-//     let encrypted = RTE(encrypt_input.value);
-
-//     // rendering result
-//     encryption_result.innerHTML = 
-//     `Encrypted: ${encrypted.Encrypted} <br> <br>
-//     RTE_key: ${encrypted.RTE_key}`;
-
-//     // 
-//     decrypt_input.value = encrypted.Encrypted;
-//     RTE_key_input.value = encrypted.RTE_key;
-// })
+const decrypt_btn = document.getElementById('decrypt_btn');
+const decrypt_input = document.getElementById('decrypt_input');
+const RTE_key_input = document.getElementById('RTE_key_input');
+const decryption_result = document.getElementById('decryption_result');
 
 
-// // 
-// decrypt_btn.addEventListener('click', ()=>{
+// 
+encrypt_btn.addEventListener('click', ()=>{
 
-//     // decrypting input
-//     let decrypted = RTD(decrypt_input.value, RTE_key_input.value);
+    // encrypting input
+    let encrypted = RTE(encrypt_input.value);
 
-//     // rendering result
-//     decryption_result.innerHTML = `Decrypted: ${decrypted}`
+    // rendering result
+    encryption_key.innerHTML = `<br> <br> RTE_key: <span class="green"> ${encrypted.RTE_key} <br> <br>`;
+    encryption_result.innerHTML = 
+    `Encrypted: <span class="red"> ${encrypted.Encrypted} </span>`;
 
-// })
+    // 
+    decrypt_input.value = encrypted.Encrypted;
+    RTE_key_input.value = encrypted.RTE_key;
+})
+
+
+// 
+decrypt_btn.addEventListener('click', ()=>{
+
+    // decrypting input
+    let decrypted = RTD(decrypt_input.value, RTE_key_input.value);
+
+    // rendering result
+    decryption_result.innerHTML = `Decrypted: ${decrypted}`
+
+})

13cf53cd963a7846270b592758d89b392e0c70b6
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-21 14:59:20 +0300

    cleaned code

--- js/RTA.js
+++ js/RTA.js
@@ -11,7 +11,6 @@ function RTE(stringToEncrypt){
     // converting input to binary
     let binaryToEncrypt = stringToBinary(stringToEncrypt);
 
-    console.log(binaryToEncrypt)
     // spliting binary 
     let segments = binaryToEncrypt.split(' ')
     
@@ -27,7 +26,6 @@ function RTE(stringToEncrypt){
         x > 0 ? x -=1 : x  = String(RTE_key).length - 1;
 
         // encryption equation
-        // segment = (parseInt(segment)* RTE_key) + parseInt(String(RTE_key).slice(x));
         segment = (parseInt(segment)) - parseInt(String(RTE_key).slice(x));
 
         // pushing hexa segments into result array
@@ -69,24 +67,12 @@ function RTD(decrypt, RTE_key){
         x > 0 ? x -=1 : x  = String(RTE_key).length -1;
 
         // decryption equation
-        // segment = (parseInt(segment)+ parseInt(String(RTE_key).slice(x))) / RTE_key ;
         segment = parseInt(segment) + parseInt(String(RTE_key).slice(x));
-
-
-        // console.log(segment);
-        // pushing string segments into result array
-        // for(bit of String(segment)){
-        //     // preventing bit overflow
-        //     if(parseInt(bit) > 1){
-        //         segment = String(segment).replace(bit, '1');
-        //     }
-        // }
         
         decrypted.push(segment);
         
     }
 
-    console.log(decrypted.join(' '))
     // joinning & converting result to string
     decrypted = binaryToString(decrypted.join(' '));
     
